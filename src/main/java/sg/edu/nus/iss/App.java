fcebe4e38476b48e969742237e4167ee2f9da06d
Yenleng <yenlengggggg@gmail.com>
2023-04-19 11:48:48 +0800

    output methods

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,6 +1,10 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,7 +34,7 @@ public class App {
         //Slide 3
         File newDirectory = new File(dirPath);
         if(newDirectory.exists()){
-            System.out.println("The directory exists.");
+            System.out.println(dirPath + " exists.");
         }else{
             newDirectory.mkdir();
             System.out.println("Directory has been created.");
@@ -41,11 +45,55 @@ public class App {
 
         File fileData = new File (dirPathFileName);
         if (fileData.exists()){
-            System.out.println("The file exists.");
+            System.out.println(dirPathFileName + " exists.");
         }else{
             fileData.createNewFile();
             
         }
+
+        //type this in terminal:
+        //c:\Users\tanye\OneDrive\Documents\Software Development Fundamentals\Day Three\day03_sdflecture\src\main\java\sg\edu\nus\iss>java sg.edu.nus.iss/App "c:\data" myfile.txt
+
+
+        //writing into file
+        String content = "I will like to go home early."; //sentence to write into file (this is input)
+        String content2 = "I will like to go home now.";
+        FileWriter fw = new FileWriter(dirPathFileName, true); //if write multiple times true = add on instead of replacing words in file
+        fw.write(content);
+        fw.write("\n");
+        fw.write(content2);
+        fw.flush();
+        fw.close(); //to release memory
+
+
+        //decorator pattern; another example to write to a file
+        String content3 = "A quick brown fox jumps over the wall.\n";
+        FileWriter fw2 = new FileWriter(dirPathFileName, true);
+        BufferedWriter bw = new BufferedWriter(fw2);
+        bw.append(content3); //take whole string and write char by char in buffer and put in file
+        bw.flush();
+        bw.close();//close bufferwriter
+        bw.close();//close filewriter
+
+
+        //another exmaple
+        String content4 = "It is good to buy a macbook with 32gb for your devlopment.\n";
+        FileOutputStream fos = new FileOutputStream(dirPathFileName, true); 
+        byte[] byteContent = content4.getBytes(); //convert string to byte cos they can only take in BYTE ARRAY args
+        fos.write(byteContent);
+        fos.flush();
+        fos.close();
+
+        //another example - with decorator pattern
+        String content5 = "I am going to buy Apple Air Tag this weekend.\n";
+        FileOutputStream fos2 = new FileOutputStream(dirPathFileName, true);
+        DataOutputStream dos2 = new DataOutputStream(fos2);
+        dos2.writeBytes(content5);
+        dos2.flush();
+        dos2.close();
+        dos2.close();
+
+
 
     }
 }
